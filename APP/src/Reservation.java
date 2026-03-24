1e684140257cf31f7f4f9818b3c5903034b50afc
Nikhil Kumar <160827660+Nikhil0xSec@users.noreply.github.com>
2026-03-24 15:04:50 +0530

    Update Reservation.java
    Removed class-level documentation for Reservation.

--- APP/src/Reservation.java
+++ APP/src/Reservation.java
@@ -1,18 +1,4 @@
 
-/**
- * ===================================================
- * CLASS - Reservation
- * ===================================================
- * Use Case 5: Booking Request (FIFO)
- *
- * Description:
- * This class represents a booking request made by a guest.
- *
- * At this stage, a reservation only captures intent,
- * not confirmation or room allocation.
- *
- * @version 5.0
- */
 public class Reservation {
 
     // Name of the guest making the booking
